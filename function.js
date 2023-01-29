efb7575d07a1aca3afd85dc8af69c1a761d0a10b
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-29 23:09:04 +0600

    function

---
+++ function.js
@@ -0,0 +1,12 @@
+function call(hi) {
+	console.loh("hi", hi);
+}
+call(2);
+
+// Function Declare
+function functionName(parameter) {
+	console.log("function");
+	console.log(parameter);
+}
+// Function Call
+functionName(200);
