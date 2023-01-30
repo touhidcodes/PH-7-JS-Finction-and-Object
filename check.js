111ae1e94f7a3f959436a630cda3159ffc942c52
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-30 19:13:12 +0600

    return

---
+++ check.js
@@ -0,0 +1,1 @@
+function check(a, b) {}
---
+++ return.js
@@ -0,0 +1,13 @@
+// Function with console
+function add(number1, number2) {
+	const sum = number1 + number2;
+	console.log(sum);
+}
+add(30, 20);
+
+// Function with return
+function adds(num1, num2) {
+	return num1 + num2;
+}
+var total2 = adds(10, 20);
+console.log(total2);
