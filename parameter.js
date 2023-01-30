ba7d8bc13b6a9280904e2b753ac7ebddf6fd298b
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-30 18:31:03 +0600

    function

---
+++ parameter.js
@@ -0,0 +1,12 @@
+function multiply(number) {
+	console.log(number);
+}
+
+multiply(3);
+
+function sum(a, b, c, d, e) {
+	const sum = a + b + c + d + e;
+	console.log(sum);
+}
+
+sum(2, 34, 5, 6, 7);
