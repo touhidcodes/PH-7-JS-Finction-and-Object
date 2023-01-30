86b060ef5b8d90fbcce3675044e138d2e48ec06f
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-30 20:04:58 +0600

    object

---
+++ object.js
@@ -0,0 +1,25 @@
+// const objectName = {property, property,...}
+const student = { id: 24, name: "touhid", class: "BSc. Hons" };
+
+// id: 24, [properties, id = key, 24 = value]
+
+const computer = {
+	name: "pc",
+	price: "24k",
+	shop: "Dhaka",
+	ram: 4,
+};
+console.log(computer);
+
+// get value
+console.log(computer.name);
+console.log(computer.shop);
+
+// set ne value
+computer.name = "mypc";
+computer.shop = "jsr";
+console.log(computer);
+
+// // All object keys and values
+// computer.key;
+// computer.value;
